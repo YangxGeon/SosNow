3ee457f47c8f8e78f7b3c2448a3d5d66b9f47c49
YangxGeon <https://github.com/YangxGeon>
2025-11-09 16:19:02 +0900

    Initial commit: Spring Boot SosNow project

---
+++ src/main/java/com/ysg/sosnow/SosNowApplication.java
@@ -0,0 +1,13 @@
+package com.ysg.sosnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ysg/sosnow/SosNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysg.sosnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
